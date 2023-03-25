57276bfe86a235ad85bbb0aa0cef5a9fef45ae56
derek <hiddenderek@gmail.com>
2023-03-25 16:27:32 -0500

    added parameter to log error

--- src/errors/logError.ts
+++ src/errors/logError.ts
@@ -1,6 +1,6 @@
 import { emitAction } from "../flowActions/emit/emitAction"
 
 export const logError = async (reason: string, id: string, stateless: boolean,  executionId?: string, requestId?: string, errors?: string) => {
-    await emitAction({name: `flow.${id}.failed`, payload: { reason, flowId: id, executionId, stateless }})   
+    await emitAction({name: `flow.${id}.failed`, payload: { reason, flowId: id, executionId, requestId, stateless }})   
     console.info(`flow '${id}' failed. Reason: ${reason}.${errors ?  ` Errors: ${errors}.` : ''}`)  
 }
